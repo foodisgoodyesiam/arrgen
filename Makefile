42ecccff6f0bc5a06357acd788eb0a3c4f89a9b2
Steven Marion <steven@dragons.fish>
2024-11-28 11:49:25 -0500

    add license and copyright info

--- Makefile
+++ Makefile
@@ -1,3 +1,19 @@
+# Copyright © 2024 Steven Marion <steven@dragons.fish>
+#
+# This file is part of arrgen.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the gnu general public license as published by
+# the free software foundation, either version 3 of the license, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but without any warranty; without even the implied warranty of
+# merchantability or fitness for a particular purpose.  see the
+# gnu general public license for more details.
+#
+# you should have received a copy of the gnu general public license
+# along with this file.  if not, see <https://www.gnu.org/licenses/>.
 
 CFLAGS ?= -O2 -flto=auto
 CFLAGS := $(CFLAGS) -MMD
--- arrgen.c
+++ arrgen.c
@@ -1,8 +1,34 @@
+/* Copyright © 2019 2024 Steven Marion <steven@dragons.fish>
+ *
+ * This file is part of arrgen.
+ *
+ * arrgen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * arrgen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with arrgen.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
+#if defined(__has_include) && __has_include(<sys/mman.h>)
+#   include <sys/mman.h>
+#   define MMAP_SUPPORTED
+#endif
 
-//static int handleBuffer(FILE* out, const uint8_t* buf, )
+static int handleFile(const char* in_path, const char* out_path);
 
 int main(int arg_num, const char** args) {
     printf("Initial commit woo!\n");
+}
+
+static int handleFile(const char* in_path, const char* out_path) {
+    //TODO
 }
